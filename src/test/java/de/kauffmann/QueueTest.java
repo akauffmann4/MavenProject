66fbf8618910d8a13d52edb3100dcd1109498d83
kauffman <aude.kauffmann@hs-furtwangen.de>
2017-12-05 15:26:57 +0100

    Andergung

--- src/test/java/de/kauffmann/QueueTest.java
+++ src/test/java/de/kauffmann/QueueTest.java
@@ -3,22 +3,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QueueTest {
-	Queue q = new Queue(3); 
+	
 	@Test
 	public void testEnqueueDequeue() {
-		
+		Queue q = new Queue(3); 
 		q.enqueue(1);
 		assertEquals(1, q.dequeue());
 		q.enqueue(1);
 		q.enqueue(2);
+		q.dequeue();
 		q.enqueue(3);
 		q.enqueue(4);
-		assertEquals(1, q.dequeue());
+		assertEquals(2, q.dequeue());
 		
 	}
 	
 	@Test(expected=IllegalStateException.class, timeout=1000)
 	public void testDequeueLeer() {
+		Queue q = new Queue(3);
 		q.dequeue();
 	}
 	
